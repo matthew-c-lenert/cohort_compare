ee26205bbcd0a802a59879fc4c9b1d5cc03ca4ef
Matt Lenert <44784114+matthew-c-lenert@users.noreply.github.com>
2019-04-24 16:56:35 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='cohort_compare',
-      version='0.1.1',
+      version='0.2.13',
       description='A library of functions for comparing clinical cohorts to the populations of US counties',
       classifiers=[
         'Development Status :: 3 - Alpha',
@@ -16,6 +16,9 @@ setup(name='cohort_compare',
       author_email='matthew.c.lenert@gmail.com',
       license='MIT',
       packages=['cohort_compare'],
+      package_data={
+        'cohort_compare': ['data/County_Data_after_impute_after_scaling.csv', 'data/us-albers-counties.json']
+        },
       install_requires=[
           'numpy>=1.15.0',
           'scipy',
@@ -23,6 +26,7 @@ setup(name='cohort_compare',
           'matplotlib',
           'fiona>=1.8.6',
           'geopandas>=0.4.1',
+          'descartes'
       ],
-      include_package_data=True,
       zip_safe=False)
+
