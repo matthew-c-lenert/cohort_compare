431a316c6d38b3856c3cb5f78b36f624e289e820
Matt Lenert <44784114+matthew-c-lenert@users.noreply.github.com>
2019-04-24 14:17:31 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,9 +3,26 @@ from setuptools import setup
 setup(name='cohort_compare',
       version='0.1',
       description='A library of functions for comparing clinical cohorts to the populations of US counties',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.'
+      ],
+      keywords='clinical study cohort similarity mapping',
       url='https://github.com/matthew-c-lenert/cohort_compare',
       author='MC Lenert',
       author_email='matthew.c.lenert@gmail.com',
       license='MIT',
       packages=['cohort_compare'],
+      install_requires=[
+          'numpy>=1.15.4',
+          'scipy',
+          'sklearn',
+          'matplotlib',
+          'fiona>=1.8.6'
+          'geopandas>=0.4.1',
+      ],
+      include_package_data=True,
       zip_safe=False)
