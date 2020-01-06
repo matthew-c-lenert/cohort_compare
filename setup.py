39acda9c8d603d4a6f89cad884333121b092e3a5
Matt Lenert <44784114+matthew-c-lenert@users.noreply.github.com>
2020-01-06 10:56:07 -0600

    update docs

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='cohort_compare',
-      version='0.2.13',
+      version='0.3.01',
       description='A library of functions for comparing clinical cohorts to the populations of US counties',
       classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Scientific/Engineering :: Medical Science Apps.'
       ],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       keywords='clinical study cohort similarity mapping',
       url='https://github.com/matthew-c-lenert/cohort_compare',
       author='MC Lenert',
